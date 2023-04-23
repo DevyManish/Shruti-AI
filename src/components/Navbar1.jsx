3358558ab94efe828c6dcac0449f6417fee295aa
Manish Gupta <121728309+DevyManish@users.noreply.github.com>
2023-04-23 18:48:29 +0530

    contact and about update

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Chatbot from '../Chatbot';
 import '../App.css';
 
+
 const Navbar1 = (props) => {
     return (
         <>
@@ -28,7 +29,7 @@ const Navbar1 = (props) => {
                                 <Link to="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
                             </li>
                             <li>
-                                <Link to="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
+                                <Link to="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
                             </li>
                             <li>
                                 <Link to="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
