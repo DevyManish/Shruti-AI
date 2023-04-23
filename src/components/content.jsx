05834e5f3626754d7ca67f030f6b06414a9e7f2f
Manish Gupta <121728309+DevyManish@users.noreply.github.com>
2023-04-23 17:27:11 +0530

    final home update

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link, Route, Routes} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function Content() {
     return (
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,18 +5,11 @@ function Footer() {
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-                    </svg>
+                <img
+                    alt="content"
+                    className="w-10 h-10 text-white p-2  rounded-full"
+                    src="https://i.postimg.cc/vBd2MN55/5cb480cd5f1b6d3fbadece79.png"
+                  />
                     <span className="ml-3 text-xl">Shruti.AI</span>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
